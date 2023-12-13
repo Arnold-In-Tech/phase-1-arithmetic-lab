fa69cfca6e6f8253d9d0bd2a864e8bdb0ffcc7a8
Arnold-In-Tech <g16a7782@gmail.com>
2023-12-13 16:11:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+
+
+//1) is an equation whose multiplied results will equal 62
+
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1*num2);
+
+
+//2) generates a random integer greater than 0
+const random = Number.parseInt(Math.random() + 1, 10);
+
+
+//3) is an equation that calculates a remainder that is equal to 4
+
+let num3 = 22;
+let num4 = 9;
+const mod = (num3 % num4);
+
+
+//4) will return 20 as the highest number in the set
+
+let max = Math.ceil(19.8);
+
+
+
+
+
